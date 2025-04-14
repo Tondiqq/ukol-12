e1c6ee3dfffe3af1413a4074c2010fc18862d361
Tondiqq <antoninstus@gmail.com>
2025-04-14 15:11:31 +0200

    2nd update, colors etc.

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const textAreaVstup = document.querySelector("#Vstup");
 const textAreaVystup = document.querySelector("#Vystup");
 
 function kodovaciFunkce(Vstup) {
-    return Vstup;
-}
+    const morseCodeMap = {
+        'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.', 'h': '....',
+        'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---', 'p': '.--.',
+        'q': '--.-', 'r': '.-.', 's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-',
+        'y': '-.--', 'z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+        '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', ' ': '/'
+    };
 
+    return Vstup.toLowerCase().split('').map(char => morseCodeMap[char] || '').join(' ');
+}
 
 textAreaVstup.onkeydown = function () {
     textAreaVystup.value = kodovaciFunkce(textAreaVstup.value);
